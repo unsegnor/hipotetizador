13bfe5c8ad72cd87888edf90bb6014d30ef5b3d3
Victor <Victor@VictorServer>
2012-12-27 21:29:46 +0100

    Muestras secuenciales también. Voy a hacer la deducción de reglas.

--- src/hipotetizador/Hipotetizador.java
+++ src/hipotetizador/Hipotetizador.java
@@ -14,25 +14,59 @@ public class Hipotetizador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         //TODO probar con una serie secuencial de intervalo uniforme
         //TODO después probar con una serie secuencial de intervalo variable
-        
-        
+        //TODO identificar entradas interesantes y filtrar el resto
+        //TODO identificar velocidad de muestreo interesante
+        //TODO identificar tamaño de ventana interesante
+        //TODO poder hacer saltar el procesamiento de la historia antes de que se llene
+        //TODO no dejar tan claros los subíndices, no deben expresar un instante fijo después, sino, un rato después, difuso
+
+
         int entradas = 4;
         int ventana = 2;
-        
+
         Hipo h = new Hipo(entradas, ventana);
-        
-        //Generar muestras
-        for(int i=0; i<100; i++){
-            
+
+        //muestras_aleatorias(h,entradas);
+        muestras_secuenciales(h,entradas);
+    }
+
+    public static void muestras_aleatorias(Hipo h, int entradas) {
+
+        //Generar muestras aleatorias
+        for (int i = 0; i < 100; i++) {
+
             //Generar muestra
             boolean muestra[] = Muestras.aleatoria(entradas);
-            
+
             //Imprimir muestra
-            System.out.println(i+"-"+Muestras.imprimir_muestra(muestra));
-            
+            System.out.println(i + "-" + Muestras.imprimir_muestra(muestra));
+
+            //Introducir muestra
+            h.muestrear(muestra);
+        }
+    }
+
+    public static void muestras_secuenciales(Hipo h, int entradas) {
+
+        //Muestra inicial
+        boolean[] muestra = new boolean[entradas];
+
+        //Inicializar la muestra
+        for (int i = 0; i < muestra.length; i++) {
+            muestra[i] = false;
+        }
+
+        for (int i = 0; i < 100; i++) {
+
+            //Generar muestra
+            muestra = Muestras.masuno_binario(muestra);
+
+            //Imprimir muestra
+            System.out.println(i + "-" + Muestras.imprimir_muestra(muestra));
+
             //Introducir muestra
             h.muestrear(muestra);
         }
--- src/hipotetizador/Muestras.java
+++ src/hipotetizador/Muestras.java
@@ -4,6 +4,7 @@
  */
 package hipotetizador;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -22,6 +23,40 @@ public class Muestras {
         for(int i=0; i<t; i++){
             muestra[i] = r.nextBoolean();
         }
+        
+        return muestra;
+    }
+    
+    public static boolean[] secuencia(int t){
+        boolean muestra[] = new boolean[t];
+        
+        //Posición a cambiar
+        int c = 0;
+        
+        
+        
+        
+        return muestra;
+    }
+    
+    public static boolean[] masuno_binario(boolean[] m){
+        boolean muestra[] = Arrays.copyOf(m, m.length);
+        
+        //Sumar uno a la primera posición y propagar el acarreo
+        
+        //Posición a cambiar
+        int c = 0;
+        while(muestra[c]){//Mientras la posición a cambiar fuera un uno hacer
+            muestra[c] = false; //Poner a cero
+            c++; //Pasar a la siguiente
+            if(c>=m.length){ //Si nos hemos pasado volvemos a empezar
+                c = 0;
+            }
+        }
+        //Después ponemos la que quede a 1 salvo que sea la última en cuyo caso lo ponemos todo a cero (ya se habrá puesto a cero)
+            muestra[c] = true;
+       
+        
         
         return muestra;
     }
